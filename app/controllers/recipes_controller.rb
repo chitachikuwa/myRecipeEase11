9c76fec32c67271e51f35647e29b2949751a7835
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-10-14 02:51:05 +0900

    コントローラファイル作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,16 @@ class RecipesController < ApplicationController
   end  
 
   def create
-  end 
-  
-  
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end  
+
+  private
+  def recipe_params
+    params.require(:recipe).pemit(:iamge, :title, :category, :ingredients, :seasonings, :step) 
+  end
 end
